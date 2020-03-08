18fea7192126637e58fc79a4d9b5ab4cb5a7090d
Naoki Yamazaki <yamazaki@kkk-c.com>
2020-03-08 21:09:36 +0900

    BTCUSD shortsの最新ローソク足を取得する

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const Discord = require("discord.js");
+const BitfinexApiWrapper = require("./lib/bitfinex-api-wrapper");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const client = new Discord.Client();
+const bfx = new BitfinexApiWrapper();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
+client.on("message", async msg => {
+  if (msg.content === "/finex") {
+    const candle = await bfx.getBtcUsdShorts();
+    if (!candle) {
+      return;
+    }
+
+    msg.reply(`\n[BTCUSD Shorts 変動通知（1時間足）]\n\n日時：${candle.day}\n\n終値\n\n\`\`\`\diff\n${candle.price}\`\`\`変動比率\n\`\`\`javascript\n${candle.ratio}%\`\`\``);
   }
 });
 
